6dd88b3648946bdf1be396192365b871489d0273
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-19 14:28:11 +0800

    RED: apply-spec works with binary functions

--- src/apply-spec.test.js
+++ src/apply-spec.test.js
@@ -7,5 +7,8 @@ describe('apply-spec', () => {
     });
     it('works with unary functions', () => {
         expect(applySpec({ v: R.inc, u: R.dec })(1)).toEqual({v: 2, u: 0})
+    });
+    it('works with binary functions', () => {
+        expect(applySpec({ sum: R.add })(1, 2)).toEqual({ sum: 3 });
     })
 })
