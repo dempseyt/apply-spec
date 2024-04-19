e37f2357ffe936331b2d49051e4f0f793536c4e4
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-19 14:28:35 +0800

    GREEN: apply-spec works with binary functions

--- src/apply-spec.js
+++ src/apply-spec.js
@@ -1,15 +1,11 @@
 function applySpec(specification) {
-    return function (value) {
-        if (Object.entries(specification).length === 0) {
-            return {};
-        }
-        let appliedSpec = {}
-        for (let [key, func] of Object.entries(specification)) {
-            appliedSpec[key] = func(value);
+    return function (value1, value2) {
+        const appliedSpec = {}
+        for (const [key, func] of Object.entries(specification)) {
+            appliedSpec[key] = func(value1, value2);
         }
         return appliedSpec;
     }
-
 }
 
 export default applySpec
