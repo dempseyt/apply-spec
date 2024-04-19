95743bee0685bac03c3cc8f403d79cef507e8ee1
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-19 14:23:28 +0800

    GREEN: apply-spec works with unary functions

--- src/apply-spec.js
+++ src/apply-spec.js
@@ -1,6 +1,13 @@
-function applySpec() {
-    return function () {
-        return {}
+function applySpec(specification) {
+    return function (value) {
+        if (Object.entries(specification).length === 0) {
+            return {};
+        }
+        let appliedSpec = {}
+        for (let [key, func] of Object.entries(specification)) {
+            appliedSpec[key] = func(value);
+        }
+        return appliedSpec;
     }
 
 }
