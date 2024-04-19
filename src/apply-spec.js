a0b11588394d5ec048d04c2ff24127aac4b013f7
dempseyt <dempsey.thompson@journeyone.com.au>
2024-04-19 14:56:25 +0800

    GREEN: apply-spec works with arrays of nested specs

--- src/apply-spec.js
+++ src/apply-spec.js
@@ -1,9 +1,15 @@
+function isObject(value) {
+
+}
+
 function applySpec(specification) {
     return function (...args) {
         const appliedSpec = {}
         for (const [key, val] of Object.entries(specification)) {
             if (typeof val === 'function') {
                 appliedSpec[key] = val(...args);
+            } else if (Array.isArray(val)) {
+                appliedSpec[key] = val.map((spec) => applySpec(spec)(...args));
             } else {
                 appliedSpec[key] = applySpec(val)(...args);
             }
